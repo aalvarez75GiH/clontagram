7ac73fcc6c7c76a2a39831d34b0816e126d73a20
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2021-09-06 10:43:56 -0400

    1st comit clonstagram class

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-export default function App() {
-  return <h1>¡Bienvenido al curso!</h1>;
+// export default function App() {
+//   return <h1>¡Bienvenido al curso!</h1>;
+// }
+
+const App = () => {
+  return <h1>Welcome to Clontagram...</h1>
 }
+
+export default App
