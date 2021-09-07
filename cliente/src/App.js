0af708525b7b67b49d0db426629de385c18a10f3
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2021-09-07 01:29:41 -0400

    5th commit - modifying signUp.js: sending data from form to Database

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -2,15 +2,12 @@ import React from 'react'
 import Nav from './components/nav'
 import SignUp from './views/signUp'
 
-// export default function App() {
-//   return <h1>¡Bienvenido al curso!</h1>;
-// }
 
 const App = () => {
   return (
   <React.Fragment>
-    <Nav />
-    <SignUp/>
+      <Nav />
+      <SignUp/>
   </React.Fragment>
 
   ) 
--- cliente/src/views/signUp.js
+++ cliente/src/views/signUp.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Main from '../components/main'
 import imagenSignUp from '../imagenes/signup.png'
+import axios from 'axios'
 
 
 const SignUp = () => {
+
+    const [ user, setUser ] = useState({
+        email: '',
+        nombre: '',
+        username: '',
+        bio: '',
+        password:''
+    })
+  
+const handleInputChange = (e) => {
+    e.preventDefault()
+    setUser(
+        {
+            ...user, [e.target.name]: e.target.value
+        }
+    )
+
+    console.log('state:', user)
+} 
+
+const handleOnSubmit = async(e) => {
+    e.preventDefault()
+    
+    try {
+        const { data } = await axios.post('/api/usuarios/signup', user)
+        console.log(data)
+
+    } catch(e){
+
+    }
+    
+    // ************* this method works properly
+    // await fetch('/api/usuarios/signup', {
+    //     method:'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(user)
+    // })
+    // .then(res =>{
+    //     console.log(res)
+    //     if (res.status === 200){
+    //         alert('User has been created successfully...')
+    //     }
+    // })
+
+
+}
+
     return (
         <Main center={ true }>
            <div className="Signup">
@@ -13,24 +63,32 @@ const SignUp = () => {
                    <p className="FormContainer__info">
                        Register with us...
                    </p>
-                   <form >
-                       <input 
+                   <form
+                   onSubmit={(e)=> handleOnSubmit(e)}
+                   >
+                       <input
+                       onChange={ (e) => handleInputChange(e)} 
                        className="Form__field" 
                        type="email" 
                        name="email" 
                        placeholder="email"
                        required
+                       value={user.email}
+                       
                        />
                        <input 
+                       onChange={ (e) => handleInputChange(e)}
                        className="Form__field" 
                        type="text" 
-                       name="fullname" 
-                       placeholder="Full name"
+                       name="nombre" 
+                       placeholder="Name"
                        required
                        minLength="3"
                        maxLength="100"
+                       value={user.name}
                        />
                        <input 
+                       onChange={ (e) => handleInputChange(e)}
                        className="Form__field" 
                        type="text" 
                        name="username" 
@@ -38,8 +96,10 @@ const SignUp = () => {
                        required
                        minLength="3"
                        maxLength="30"
+                       value={user.username}
                        />
                        <input 
+                       onChange={ (e) => handleInputChange(e)}
                        className="Form__field" 
                        type="text" 
                        name="bio" 
@@ -47,13 +107,16 @@ const SignUp = () => {
                        required
                        minLength="3"
                        maxLength="150"
+                       value={user.bio}
                        />
-                       <input 
+                       <input
+                       onChange={ (e) => handleInputChange(e)} 
                        className="Form__field" 
                        type="password" 
                        name="password" 
                        placeholder="password"
                        required
+                       value={user.password}
                        />
                        <button 
                        className="Form__submit" 
